2d085d53b777fcd9f38ca34ebacc65d8d680ce25
Ysreimer Ortiz <ysreimer@gmail.com>
2023-12-18 00:17:05 -0500

    18-12-2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 
 import './App.css'
 import '/src/style/media.css'
-import Listado from './components/listado'
-import Nav from './components/nav'
 import Layout from './layout/Layout'
 
 function App() {
 
   return (
     <>
-    {/* <Nav/> */}
-    {/* <Listado/> */}
     <Layout/>
     </>
   )
